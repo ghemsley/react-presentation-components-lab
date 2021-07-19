93f2ff222fc75ebe59a51392fb23b5c4e31c635b
ghemsley <ghemsley@protonmail.ch>
2021-07-19 15:02:43 -0500

    commit tree 7e5406db60dbe1c1152be074a5f58923f956d75e

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
+import React, { Component } from 'react'
+
 // Code SimpleComponent Here
+class SimpleComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
+  handleClick = () => {
+    this.setState(
+      this.state.mood === 'happy' ? { mood: 'sad' } : { mood: 'happy' }
+    )
+  }
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>
+  }
+}
+
+export default SimpleComponent
